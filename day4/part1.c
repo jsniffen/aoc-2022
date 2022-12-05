157240cab5217f6fb9040f66f9e97b7694033a62
Julian Sniffen <julian.sniffen@gmail.com>
2022-12-05 01:47:44 -0500

    day4

---
+++ day4/part1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char numbers[4];
+static char number[16];
+static int number_size;
+
+int parse_line()
+{
+    char c;
+    int i = 0;
+
+    while (1) {
+        c = getchar();
+        if (c == EOF) return 0;
+        if (c == '-' || c == ',' || c == '\n') {
+            number[number_size++] = '\0';
+            numbers[i++] = atoi(number);
+            number_size = 0;
+
+            if (c == '\n') {
+                return 1;
+            }
+        } else {
+            number[number_size++] = c;
+        }
+    }
+}
+
+int main()
+{
+    int count = 0;
+    while(parse_line()) {
+        if ((numbers[0] <= numbers[2] && numbers[1] >= numbers[3]) || 
+            (numbers[2] <= numbers[0] && numbers[3] >= numbers[1])) {
+            count += 1;
+        }
+    }
+    printf("The count of self-contained pairs is: %d\n", count);
+}
